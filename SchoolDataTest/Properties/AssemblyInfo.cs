e8230df72990ec1de58e6b78440abc3c36c35737
gokulrayar <gokulrayar>
2024-01-11 19:16:21 +0530

    con unit

---
+++ SchoolDataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchoolDataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchoolDataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94dd0703-df92-4252-b731-deca42207feb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SchoolDataTest/SchoolUnitTest.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using schoolLib;
+using System;
+using Assert = NUnit.Framework.Assert;
+
+namespace SchoolDataTest
+{
+
+
+
+    [TestFixture]
+    public class SchoolUnitTest
+    {
+        [Test]
+        public void AddStudent()
+        {
+            var schoolManagement = new SchoolManagement();
+            var student = new Student { StudentId = 1, StudentName = "rayar", Class = "Class A" };
+
+            // Act
+            schoolManagement.AddStudent(student);
+
+            // Assert
+            Assert.Contains(student, schoolManagement.Students);
+        }
+        [Test]
+        public void AddSubject()
+        {
+            var schoolManagement = new SchoolManagement();
+            var subject = new Subject { SubjectId = 1, SubjectName = "Math" };
+
+            // Act
+            schoolManagement.AddSubject(subject);
+
+            // Assert
+            Assert.Contains(subject, schoolManagement.Subjects);
+        }
+        [Test]
+        public void AddTeacher()
+        {
+            var schoolManagement = new SchoolManagement();
+            var teacher = new Teacher { TeacherId = 1, TeacherName = "Mr. Ashok" };
+
+            // Act
+            schoolManagement.AddTeacher(teacher);
+
+            // Assert
+            Assert.Contains(teacher, schoolManagement.Teachers);
+        }
+        [Test]
+        public void StudentMockTest()
+        {
+            var schoolManagement = new SchoolManagement();
+            var studentMock = new Mock<Student>();
+            studentMock.SetupGet(s => s.StudentId).Returns(1);
+            studentMock.SetupGet(s => s.StudentName).Returns("rayar");
+            studentMock.SetupGet(s => s.Class).Returns("Class A");
+
+            // Act
+            schoolManagement.AddStudent(studentMock.Object);
+
+            // Assert
+            Assert.Contains(studentMock.Object, schoolManagement.Students);
+        }
+        [Test]
+        public void TeacherMockTest()
+        {
+            var schoolManagement = new SchoolManagement();
+            var teacherMock = new Mock<Teacher>();
+            teacherMock.SetupGet(s => s.TeacherId).Returns(1);
+            teacherMock.SetupGet(s => s.TeacherName).Returns("Mr.shanks");
+
+
+            // Act
+            schoolManagement.AddTeacher(teacherMock.Object);
+
+            // Assert
+            Assert.Contains(teacherMock.Object, schoolManagement.Teachers);
+        }
+        [Test]
+        public void SubjectMockTest()
+        {
+            var schoolManagement = new SchoolManagement();
+            var subjectMock = new Mock<Subject>();
+            subjectMock.SetupGet(s => s.SubjectId).Returns(1);
+            subjectMock.SetupGet(s => s.SubjectName).Returns("Math");
+
+
+            // Act
+            schoolManagement.AddSubject(subjectMock.Object);
+
+            // Assert
+            Assert.Contains(subjectMock.Object, schoolManagement.Subjects);
+        }
+    }
+}
